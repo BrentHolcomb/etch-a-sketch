f1320ccfcbd1c8f815f7b6638d763f93b958723d
Brent Holcomb <brentjholcomb@gmail.com>
2022-05-25 20:33:53 -0400

    add random colors

--- script.js
+++ script.js
@@ -24,26 +24,58 @@ function grid() {
     }
 }
 
+function addHover (boxNumber) {
+    let randomColor;
+    let selectedRow;
+    for (let i = 1; i <= boxNumber; i++) {
+        // add unique ID to each row
+        selectedRow = document.querySelector(`.rows:nth-child(${i})`);
+        selectedRow.setAttribute("id", `row${i}`);
+
+        // add event listener for each tile in row
+        for (let n = 1; n <= boxNumber; n++) {
+            let hover = document.querySelector(`#row${i} div:nth-child(${n})`);
+            hover.addEventListener("mouseover", () => {
+                hover.classList.add("hover");
+
+                // adds random color class
+                randomColor = Math.floor(Math.random() * 6);
+                hover.classList.add(`color${randomColor}`);
+            });
+            hover.addEventListener("mouseout", () => {
+                setTimeout(() => {hover.classList.remove("hover", "color0", "color1", "color2", "color3", "color4", "color5")}, 500)
+            });
+        }
+    }
+}
+
+// initial values
 let boxNumber = 16;
 let tileSize = 45 / boxNumber;
 
+// initial grid
 grid();
+addHover(boxNumber);
 
-// add class on mouseover, remove class on mouseout for each tile
-let selectedRow;
-for (let i = 1; i <= boxNumber; i++) {
-    // add unique ID to each row
-    selectedRow = document.querySelector(`.rows:nth-child(${i})`);
-    selectedRow.setAttribute("id", `row${i}`);
 
-    // add event listener for each tile in row
-    for (let n = 1; n <= boxNumber; n++) {
-        let hover = document.querySelector(`#row${i} div:nth-child(${n})`);
-        hover.addEventListener("mouseover", () => {
-            hover.classList.add("hover");
-        });
-        hover.addEventListener("mouseout", () => {
-            hover.classList.remove("hover");
-        });
-    }
-}
+const button = document.querySelector("button");
+const entry = document.querySelector("input");
+let text;
+
+button.addEventListener("click", () => {
+    text = entry.value;
+    text = +text;
+    if (typeof text === "number" && text <= 100) {
+        // removes old rows and tiles
+        for (let i = 0; i < boxNumber; i++) {
+            const rowsToRemove = document.querySelector(".rows");
+            if (rowsToRemove) {rowsToRemove.remove()}
+        }
+        
+        boxNumber = text;
+        tileSize = 45 / boxNumber;
+
+        grid();
+        addHover(boxNumber);
+    } else {alert("Type a number that is 100 or below")}
+});
